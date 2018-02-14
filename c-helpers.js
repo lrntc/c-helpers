de1aee00c29b4da32001c45078ccfd1eef47440d
Laurent Constant <laurent.constant@bbdo.be>
2018-02-14 10:50:30 +0100

    refzctoring and new functions

--- c-helpers.js
+++ c-helpers.js
@@ -1,143 +1,150 @@
 var _c = {
-    //
-    //Base
-    //
-    print: function(obj) {
+  //
+  //Base
+  //
+  print: function(obj) {
 
-        console.log(obj);
+    console.log(obj);
 
-    },
-    parseInt: (value, radix = 10) => { //Safely parse an integer using the radix method source: https://eslint.org/docs/rules/radix
-      return parseInt(value, radix);
-    },
-    each: function(querySelector, callback) { //Callback function should have following params: element, index
+  },
+  table: function(obj) {
 
-        let elements = document.querySelectorAll(querySelector);
-        Array.prototype.forEach.call(elements, callback);
+    console.table(obj);
 
-    },
-    eachString: function(arrayCollection, callback) {
+  },
+  parseInt: (value, radix = 10) => { //Safely parse an integer using the radix method source: https://eslint.org/docs/rules/radix
+    return parseInt(value, radix);
+  },
+  each: function(querySelector, callback) { //Callback function should have following params: element, index
 
-        Array.prototype.forEach.call(arrayCollection, callback);
+    let elements = document.querySelectorAll(querySelector);
+    console.log(elements)
+    callback(elements);
 
-    },
-    eachArrayObject: function(arrayCollection, callback) {
+  },
+  eachString: function(arrayCollection, callback) {
 
-        Array.prototype.forEach.call(arrayCollection, callback);
+    Array.prototype.forEach.call(arrayCollection, callback);
 
-    },
-    elementExcists: function(querySelector, callback = null){
+  },
+  eachArrayObject: function(arrayCollection, callback) {
 
-        if(document.getElementById(querySelector)) {
+    Array.prototype.forEach.call(arrayCollection, callback);
 
-            return true;
+  },
+  elementExcists: function(querySelector){
 
-        }
+    if(document.getElementById(querySelector)) {
 
-        return false;
+      return true;
 
-    },
-    classExcists: function(querySelector){
-
-        if(document.querySelectorAll(querySelector).length != 0) {
-
-            return true;
-
-        }
-
-        return false;
-
-    },
-    addEventListener: function(querySelector, eventName, callback) { //Add an event listener to a single element
-
-        let target = document.getElementById(querySelector);
-
-        if(target) {
-
-            target.addEventListener(eventName, callback);
-
-        } else {
-            this.print(querySelector + ' not found in DOM');
-        }
-
-    },
-    addEventListeners: function(querySelector, eventName, callback) { //Add an event listener to a multiple elements query
-
-        let targets = document.querySelectorAll(querySelector);
-
-        if(targets) {
-
-            for (let i = 0; i < targets.length; i++) {
-                targets[i].addEventListener(eventName, callback);
-            }
-
-        } else {
-            this.print(querySelector + ' not found in DOM');
-        }
-
-    },
-    //
-    //DOM manipalation
-    //
-    clearElement: function(target) {
-
-        target.innerHTML = '';
-
-    },
-    appendHtmlElement: function(target, htmlString) {
-
-        let div = document.createElement('div');
-        div.innerHTML = htmlString.trim();
-        target.appendChild(div.firstChild);
-
-    },
-    appendSelectOption: function(target, obj) {
-
-        let elm = document.getElementById(target);
-        let option = document.createElement("option");
-        option.value = obj.value;
-        option.text = obj.text;
-        elm.add(option);
-
-    },
-    // appendHtmlElements: function (htmlString) {
-    //     var div = document.createElement('div');
-    //     div.innerHTML = htmlString.trim();
-    //
-    //     return div.childNodes;
-    // },
-
-    //
-    //WWW
-    //
-    getParameterByName: function (name, url) {
-        if (!url) url = window.location.href;
-        name = name.replace(/[\[\]]/g, "\\$&");
-        var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
-        if (!results) return null;
-        if (!results[2]) return '';
-        return decodeURIComponent(results[2].replace(/\+/g, " "));
-    },
-    //
-    //Effects
-    //
-    scrollTo: function(element, to, duration) { //Thanks to https://gist.github.com/andjosh/6764939
-        var start = element.scrollTop,
-        change = to - start,
-        currentTime = 0,
-        increment = 20;
-
-        var animateScroll = function(){
-            currentTime += increment;
-            var val = Math.easeInOutQuad(currentTime, start, change, duration);
-            element.scrollTop = val;
-            if(currentTime < duration) {
-                setTimeout(animateScroll, increment);
-            }
-        };
-        animateScroll();
     }
+
+    return false;
+
+  },
+  classExcists: function(querySelector){
+
+    if(document.querySelectorAll(querySelector).length != 0) {
+
+      return true;
+
+    }
+
+    return false;
+
+  },
+  addEventListener: function(querySelector, eventName, callback) { //Add an event listener to a single element
+
+    let target = document.getElementById(querySelector);
+
+    if(target) {
+
+      target.addEventListener(eventName, callback);
+
+    } else {
+      this.print(querySelector + ' not found in DOM');
+    }
+
+  },
+  addEventListeners: function(querySelector, eventName, callback) { //Add an event listener to a multiple elements query
+
+    let targets = document.querySelectorAll(querySelector);
+
+    if(targets) {
+
+      for (let i = 0; i < targets.length; i++) {
+        targets[i].addEventListener(eventName, callback);
+      }
+
+    } else {
+      _c.print(querySelector + ' not found in DOM');
+    }
+
+  },
+  //
+  //DOM manipalation
+  //
+  clearElement: function(target) {
+
+      target.innerHTML = '';
+
+  },
+  appendHtmlElement: function(target, htmlString) {
+
+    let wrapper = document.createElement('div');
+    wrapper.innerHTML = htmlString.trim();
+    target.appendChild(wrapper.firstChild);
+
+  },
+  appendHtmlElements: function(target, htmlString, classNames = '') {
+
+    let wrapper = document.createElement('div');
+    wrapper.className += classNames;
+    wrapper.innerHTML = htmlString.trim();
+    target.appendChild(wrapper);
+
+  },
+  appendSelectOption: function(target, obj) {
+
+    let elm = document.getElementById(target);
+    let option = document.createElement("option");
+    option.value = obj.value;
+    option.text = obj.text;
+    elm.add(option);
+
+  },
+  //
+  //WWW
+  //
+  getParameterByName: function (name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+  },
+  //
+  //Effects
+  //
+  scrollTo: function(element, to, duration) { //Thanks to https://gist.github.com/andjosh/6764939
+    var start = element.scrollTop,
+    change = to - start,
+    currentTime = 0,
+    increment = 20;
+
+    var animateScroll = function(){
+      currentTime += increment;
+      var val = Math.easeInOutQuad(currentTime, start, change, duration);
+      element.scrollTop = val;
+      if(currentTime < duration) {
+        setTimeout(animateScroll, increment);
+      }
+    };
+    animateScroll();
+  }
 }
 
 
@@ -147,10 +154,10 @@ var _c = {
 //c = change in value
 //d = duration
 Math.easeInOutQuad = function (t, b, c, d) {
-    t /= d/2;
-    if (t < 1) return c/2*t*t + b;
-    t--;
-    return -c/2 * (t*(t-2) - 1) + b;
+  t /= d/2;
+  if (t < 1) return c/2*t*t + b;
+  t--;
+  return -c/2 * (t*(t-2) - 1) + b;
 };
 
 
